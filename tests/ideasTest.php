6eb967536c9ed0a7f4d1c307852b208df7731e29
Tetiana Hnativ <tetianahnativ2003@gmail.com>
2024-06-16 10:58:30 +0300

    tests

---
+++ tests/ideasTest.php
@@ -0,0 +1,20 @@
+<?php
+ob_start();
+
+include '../phpDatabase/ideasDatabase.php';
+
+$json_data = ob_get_clean();
+
+$data_array = json_decode($json_data, true);
+
+if (!empty($data_array)) {
+    echo "Data received successfully!<br>";
+    foreach ($data_array as $item) {
+        echo "<br>"; 
+        foreach ($item as $key => $value) {
+            echo $key . ": " . $value . "<br>"; 
+        }
+    }
+} else {
+    echo "No results found!\n";
+}
